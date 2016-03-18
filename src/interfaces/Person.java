3e1c29c951494c6086f62830e4b56fde42606640
LuukvH <laj@home.nl>
2016-03-18 16:51:21 +0100

    Implemented woman class

---
+++ src/interfaces/Person.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+/**
+ * Created by laj on 18-3-2016.
+ */
+public interface Person {
+
+    boolean canDrive();
+
+    int age();
+
+
+}
---
+++ src/models/Woman.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by laj on 18-3-2016.
+ */
+public class Woman {
+}
